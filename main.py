76d96e41f24657e61f2e662d139d0458f9a90035
broccoli-robbie <broccolirobbie@proton.me>
2025-08-06 20:38:28 -0400

    added verbose flag

--- main.py
+++ main.py
@@ -8,18 +8,35 @@ load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 prompt = sys.argv
-messages = [
-    types.Content(role="user", parts=[types.Part(text=prompt)]),
-]
 
 if len(prompt) == 1:
-    print("Error")
+    print("Error: No prompt/arguments given.")
     sys.exit(1)
-else:
+elif len(prompt) == 2:
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=prompt[1])]),
+    ]
+
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
         contents=messages,
     )
-    print(
-        f"Response: {response.text}\nPrompt tokens: {response.usage_metadata.prompt_token_count}\nResponse tokens: {response.usage_metadata.candidates_token_count}"
-    )
+
+    print(f"Response: {response.text}")
+
+elif len(prompt) == 3:
+    if prompt[2] == "--verbose":
+        messages = [
+            types.Content(role="user", parts=[types.Part(text=prompt[1])]),
+        ]
+
+        response = client.models.generate_content(
+            model="gemini-2.0-flash-001",
+            contents=messages,
+        )
+
+        print(
+            f"User prompt: {prompt[1]}\n\nResponse: {response.text}\nPrompt tokens: {response.usage_metadata.prompt_token_count}\nResponse tokens: {response.usage_metadata.candidates_token_count}"
+        )
+    else:
+        print("Error: Invalid argument given with prompt.")
